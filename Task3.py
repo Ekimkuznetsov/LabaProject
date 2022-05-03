60665cb365255595f4f8741bf9840041b864bc8c
mike <ekimkuznetsov@gmail.com>
2022-05-03 21:12:34 +0300

    Task 6.3  (start) String method added

---
+++ Task3.py
@@ -0,0 +1,15 @@
+# String generator
+import string
+alphabet_list = list(string.ascii_lowercase)
+print(alphabet_list)
+#Password generator
+#Start
+
+#Len of password
+#Format of password
+#Manual format
+
+
+#Pattern
+#Password generated
+#Log
