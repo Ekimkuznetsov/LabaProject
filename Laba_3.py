4e609a95a70d3e1549aa09a4c15f5869f8702c9e
mike <ekimkuznetsov@gmail.com>
2022-05-05 21:04:49 +0300

    Task 6.3  (start) Close to finish generator

--- Laba_3.py
+++ Laba_3.py
@@ -1,8 +1,8 @@
 # Task3
 import string
 import random
-import re
 
+#Constants. Tokens groups
 alphabet_small = list(string.ascii_lowercase)
 alphabet_big = list(string.ascii_uppercase)
 digits_list = list(string.digits)
@@ -12,52 +12,61 @@ punctuation_list = list(string.punctuation)
 #print(alphabet_big)
 #print(digits_list)
 #print(punctuation_list)
+
 # 1 Default password generation
-def raw_gen(format = "A4%d3%-%a2"):
+#Function to make tokens_list from raw_tokens
+def tokens_l(raw_tokens = "A4%d3%-%a2"):
     tokens = ["d", "A", "a", "p", "-", "@"]
     tokens_list = []
 
     try:
-        "[" in format
-        a = int(format.index("["))
-        b = int(format.index("]"))
-        print(a, b)
-        c = format[a + 1 : b]
-        print(c)
-
-        d = c.replace("%", "")
-        print(d)
-         =
+        if raw_tokens.endswith("%"):
+            raw_tokens = raw_tokens[:-1]
     except:
         pass
 
-
     try:
-        if format.endswith("%"):
-            format = format[:-1]
+        "[" in raw_tokens
+        a = int(raw_tokens.index("["))
+        b = int(raw_tokens.index("]"))
+        print(a, b)
+        c = raw_tokens[a + 1 : b]
+        print(c)
+        d = c.replace("%", "")
+        print(d)
+        raw_tokens = raw_tokens.replace(raw_tokens[a:b+1], d)
+    except:
+        pass
+    raw_tokens = raw_tokens.split("%")
+    print("Raw_tokens: ", raw_tokens)
+    for token in raw_tokens:
 
-    finally:
-        raw_tokens = format.split("%")
-        print("Raw_tokens: ", raw_tokens)
-
-    for item in raw_tokens:
+        type_token = ''
+        i = 0
         try:
-            count = int(item[1:])
+            while token[i] in tokens: type_token += token[i]; i +=1
         except:
-            if item[1:] == "":
+            if type_token == '': print('Wrong template key : ', token); break
+
+        try: count = int(token[i:])
+        except:
+            if type_token in tokens:
                 count = 1
-        try:
-            item[0] in tokens
-            type_token = item[0]
-        except:
-            type_token = None
-            count = None
-
-        block = type_token * count
+            else: print('Wrong template key : ', token); break
+        print('num is : ', count)
+#-----------------------------------------------------------------------------
+        block = ""
+        if len(type_token) > 1:
+            for i in range(count):
+                if
+                block += random.choice(list)
+        else:
+            block = count * type_token
         tokens_list += block
-        #print("There is a block: ", block)
+        print("There is a block: ", block)
     print("There is your tokens list: ", tokens_list)
     return(tokens_list)
+
 
 def password_gen(raw_gen):
     pas = ''
@@ -76,7 +85,7 @@ def password_gen(raw_gen):
             pas += "@"
         #else: print("Wrong token")
     print("Your password is: ", pas)
-password_gen(raw_gen("A4%[d%a%]3%-%a2%"))
+password_gen(tokens_l("A2%[d%a%]3%-%a2%"))
 
 
 # 2 Password generation of the set length
--- Task3.py
+++ Task3.py
@@ -1,15 +1,59 @@
-# String generator
 import string
-alphabet_list = list(string.ascii_lowercase)
-print(alphabet_list)
-#Password generator
-#Start
-
-#Len of password
-#Format of password
-#Manual format
+import random
 
 
-#Pattern
-#Password generated
-#Log
+def genPassword(pTemplate):
+    def sList(key):
+        list = ''
+        for s in key:
+            if s == 'a': list += string.ascii_lowercase
+            elif s == 'A': list += string.ascii_uppercase
+            elif s == 'd': list += string.digits
+            elif s == 'p': list += string.punctuation
+            elif s == '-': list += '-'
+            elif s == '@': list += '@'
+        # print('List : ', list)
+        return list
+
+    def genPart(list, num):
+        p = ''
+        for i in range(num): p += random.choice(list)
+        # print('password part : ', p)
+        return p
+
+    keys = ['A', 'a', 'd', 'p', '-', '@']
+    tokenlist = pTemplate.split("%")
+    print('Tokens : ', tokenlist)
+    password = ''
+    for token in tokenlist:
+        key = ''
+        i = 0
+        try:
+            while token[i] in keys: key += token[i]; i +=1
+        except:
+            if key == '': print('Wrong template key : ', token); break
+        #print('key is : ', key)
+        #print('i = ', i)
+
+        try: n = int(token[i:])
+        except:
+            if key in keys: n=1
+            else: print('Wrong template key : ' ,token); break
+        #print('num is : ', n)
+        password += genPart(sList(key),n)
+    print('Password : ', password)
+    return password
+
+if __name__ == 'main':
+    #pTemplate = input("Input template: ")
+    #pTemplate = 'A4%da3%-%a2%'
+    #pTemplate = 'Aa4%@%d3%-%a2%p4%'
+    pTemplate = 'Aa6%@%[d%p%]3%-%a2'
+    print('Start Template :' , pTemplate)
+    find = '['+ pTemplate.partition('[')[2].partition(']')[0] + ']'
+    print('Sub template : ', find)
+    replace = pTemplate.partition('[')[2].partition(']')[0].replace('%','')
+    print('Replace part: ', replace)
+    pTemplate = pTemplate.replace(find,replace)
+    print('Password template : ', pTemplate)
+    genPassword(pTemplate)
