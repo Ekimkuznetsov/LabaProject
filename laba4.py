0d6df1198820e8156581c1ee0a3cd0f85ddbedbf
mike <ekimkuznetsov@gmail.com>
2022-05-11 21:21:13 +0300

    Laba4 import csv

--- laba4.py
+++ laba4.py
@@ -1,4 +1,8 @@
-
+import csv
+with open('access_log.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
 
 
 
@@ -8,12 +12,14 @@ if __name__ == "__main__":
 
 
 
-#Task4
+ '''
+
 #Файл CSV должен содержать следующие колонки:
-columns = (["MAC address", "hostname",
+#columns = (["MAC address", "hostname",
            "IPv4(null)", "IPv6(null)",
            "netmask(xxx.xxx.xxx.xxx)", "user login",
            "full user name", "email",
            "ssh private key", "ssh public key",
            "description host", "list of installed app",
            "UUID"])
+'''
