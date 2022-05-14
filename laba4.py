a3f30bc705ffb25f2ed0a0a43ddab8a1854c6d41
mike <ekimkuznetsov@gmail.com>
2022-05-14 18:03:08 +0300

    Laba4  csv files with brackets red1

--- laba4.py
+++ laba4.py
@@ -1,21 +1,24 @@
 import csv
 from io import StringIO
-
-converted = StringIO()
-with open('access_log1.csv', 'rt') as f:
-    converted.write(f.read().replace('(', ']').replace(')', ']').replace('[', ']').replace('"', ']'))
-    csv_reader = csv.reader(converted, doublequote=True, delimiter=" ", quotechar=']')
-
-converted.seek(0)
-count = 1
-for line in csv_reader:
-    print(line)
-    count += 1
-    if count == 10:
-        break
+#For the case if the file not big and it is NO NESTED BRACKETS
+#Creating new file object
+def dsv_csv():
+    converted = StringIO()
+    with open('access_log1.csv', 'rt') as f:
+        converted.write(f.read().replace('(', ']').replace(')', ']').replace('[', ']').replace('"', ']'))
+        csv_reader = csv.reader(converted, doublequote=True, delimiter=" ", quotechar=']')
+    converted.seek(0) #Set the cursor at index 0
+    #10 lines print
+    count = 1
+    for line in csv_reader:
+        print(line)
+        count += 1
+        if count == 10:
+            break
 
 
 if __name__ == "__main__":
+    dsv_csv()
     print("Let's go!")
 
 
