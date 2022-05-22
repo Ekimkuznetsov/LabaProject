17efba9e69b67d91d619f06c09a45f7bd44966b2
mike <ekimkuznetsov@gmail.com>
2022-05-22 21:30:08 +0300

    Laba4 filtering initial function

--- laba4.py
+++ laba4.py
@@ -1,6 +1,6 @@
 import csv
 import argparse
-#import logging
+import re
 #from datetime import datetime
 
 
@@ -41,7 +41,6 @@ def with_header(file = 'ac.csv', newfile = 'new_ac.csv'):
         dialect = csv.Sniffer().sniff(csv_file.read(2048)) #To recognise a dialect of csv file
         csv_file.seek(0) #Move to  the start of the file
         reader = csv.reader(csv_file, dialect)
-        header = []
         header = next(reader)
         csv_file.seek(0)
         csv_reader = csv.DictReader(csv_file, dialect=dialect) #Creation of Reader as a dictionary with dialect
@@ -69,9 +68,7 @@ def start():
 
 
 def row_tables_select(fileI='ac.csv', fileO='new_ac.csv', delimiter=",", r1=None, r2=None, c1=None, c2=None):
-    print(fileI, fileO, delimiter, r1, r2, c1, c2)
-    #files, rows, columns
-
+    print(r1, r2, c1, c2)
     with open (fileI) as source:
         with open (fileO, "w") as dest:
             reader = csv.reader(source, delimiter=';')
@@ -79,37 +76,63 @@ def row_tables_select(fileI='ac.csv', fileO='new_ac.csv', delimiter=",", r1=None
             for row in list(reader)[r1:r2]:
                 writer.writerow(row[c1:c2])
 
-def rows_and_columns(args):
-    files = args.file.split(',')
-    if args.rows and args.columns:
-        rows = args.rows.split(',')
-        r1, r2 = int(rows[0]), int(rows[1])
-        columns = args.columns.split(',')
-        c1, c2 = int(columns[0]), int(columns[1])
-        row_tables_select(fileI=files[0], fileO=files[1], delimiter=args.delimiter, r1=r1, r2=r2, c1=c1, c2=c2)
-        print(r1, r2, c1, c2)
-    elif args.columns:
-        columns = args.columns.split(',')
-        c1, c2 = int(columns[0]), int(columns[1])
-        row_tables_select(fileI=files[0], fileO=files[1], delimiter=args.delimiter, c1=c1, c2=c2)
-        print(c1, c2)
-    else:
-        if args.rows:
-            rows = args.rows.split(',')
-            r1, r2 = int(rows[0]), int(rows[1])
-            row_tables_select(fileI=files[0], fileO=files[1], delimiter=args.delimiter, r1=r1, r2=r2)
-            print(r1, r2)
 
-#Check for the arguments
-def myParser(args):
+def extraction(args):
     files = args.file.split(',')
     if args.rows or args.columns:
-        rows_and_columns(args)
-    else:
-        if args.delimiter:
-            row_tables_select(fileI=files[0], fileO=files[1], delimiter=args.delimiter)
+        if args.rows:
+            rows = args.rows.split(',')
+            if args.columns:
+                columns = args.columns.split(',')
+                row_tables_select(fileI=files[0], fileO=files[1], r1=int(rows[0]), r2=int(rows[1]), c1=int(columns[0]), c2=int(columns[1]))
+            else:
+                row_tables_select(fileI=files[0], fileO=files[1], r1=int(rows[0]), r2=int(rows[1]))
         else:
-            row_tables_select(fileI=files[0], fileO=files[1])
+            columns = args.columns.split(',')
+            row_tables_select(fileI=files[0], fileO=files[1], c1=int(columns[0]), c2=int(columns[1]))
+        print('r1, r2, c1, c2')
+    else:
+        row_tables_select(fileI=files[0], fileO=files[1])
+
+def filtering(args):
+    params = args.pattern.split(',')
+    files = args.file.split(',')
+    #pattern = params[1]
+    #columns or rows
+    if params[0] == 'c':
+        index = int(args.columns.split(',')[0])
+        with open(files[0]) as source:
+            with open(files[1], "w") as dest:
+                reader = csv.reader(source, delimiter=';')
+                writer = csv.writer(dest)
+                for row in list(reader):
+                    if re.match(r'[0-2]+', row[index]):
+                        writer.writerow(row)
+    #by rows [0-9]+(\.[0-9]+){3}
+    else:
+        index = int(args.rows.split(',')[0])
+        with open(files[0]) as source:
+            with open(files[1], "w") as dest:
+                reader = csv.reader(source, delimiter=';')
+                writer = csv.writer(dest)
+                for row in list(reader):
+                    if re.match(r'[0-2]+', row):
+
+                        writer.writerow(row[2 - 5])
+
+
+
+
+def myParser(args):
+    if args.extraction:
+        extraction(args)
+    elif args.filtering:
+        filtering(args)
+
+
+
+
+
 
 
 
@@ -125,7 +148,12 @@ if __name__ == "__main__":
     #logger = logging.getLogger()
     # My parser variable
     my_parser = argparse.ArgumentParser()
-    # Arguments set
+    #creation of mutually exclusive group
+    my_group = my_parser.add_mutually_exclusive_group(required=False)
+    my_group.add_argument('-e', '--extraction', action='store_true', help="Extract special columns or rows. Used with -c, -r")
+    my_group.add_argument('-s', '--filtering', action='store_true', help="To use filter through a file. Used with -p")
+
+    my_parser.add_argument('-p', '--pattern', action='store', type=str, help="Pattern to look for in format c(or r),regex")
     my_parser.add_argument('-f', '--file', action='store', type=str, help="Source file, destination file", required=True)
     my_parser.add_argument('-d', '--delimiter', action='store', type=str, help="Specify delimiter next way: '\t' for tab, ';' for ';' etc")
     my_parser.add_argument('-r', '--rows', action='store', type=str, help="Specify rows: '1, 2' Use ',' as delimiter ")
@@ -134,7 +162,8 @@ if __name__ == "__main__":
     #Execute the parse_args() method
     args = my_parser.parse_args()
     #Start of the program
-    myParser(args)
+    #myParser
+    filtering(args)
 
 
 
@@ -142,15 +171,15 @@ if __name__ == "__main__":
 if __name__ == '__main__':
     
 
-    
+    #Arguments set
     
     1) Записать csv в нужном формате____done
     2) Выбока по столбцам_______________done
     3) Выбока по строкам________________done
-    4) Фильтр по столбцу________________
-    5) Фильтр по строке_________________
-    6) Разделитель______________________
-    7) Файл_____________________________done
+    4) Фильтр по столбцу
+    5) Фильтр по строке
+    6) Разделитель
+    7) Файл
 
 
 
