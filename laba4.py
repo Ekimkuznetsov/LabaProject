f0857c03254a727ede83e990ad1594610a5074e5
mike <ekimkuznetsov@gmail.com>
2022-05-26 16:48:07 +0300

    Laba4 file done

--- laba4.py
+++ laba4.py
@@ -1,11 +1,10 @@
 import csv
 import argparse
 import re
-#from datetime import datetime
+# from datetime import datetime
 
 
-#good to make property
-
+# good to make property
 def reader_dialected(file='ac.csv'):
     # Reader creation
     with open(file) as csv_file:
@@ -14,52 +13,63 @@ def reader_dialected(file='ac.csv'):
         reader = csv.reader(csv_file, dialect)
         return reader
 
-#Function to write csv in ',' format
-def without_header(file = 'ac.csv', newfile = 'new_ac.csv'): #default files names to simplify
-    #Reader creation
+
+# Function to write csv in ',' format
+def without_header(file='ac.csv', newfile='new_ac.csv'):  # default files names to simplify
+    # Reader creation
     with open(file) as csv_file:
-        dialect = csv.Sniffer().sniff(csv_file.read(1024)) #To recognise a dialect of csv file
-        csv_file.seek(0) #Move to  the start of the file
+        dialect = csv.Sniffer().sniff(csv_file.read(1024))  # To recognise a dialect of csv file
+        csv_file.seek(0)  # Move to  the start of the file
         reader = csv.reader(csv_file, dialect)
 
-        #Writing to the new file
+        # Writing to the new file
         with open(newfile, 'w') as new_file:
             csv_writer = csv.writer(new_file)
             for line in reader:
                 csv_writer.writerow(line)
     print('Without header')
 
-#check: Is csv file has header
-def header_check(file = 'ac.csv'):
+
+# check: Is csv file has header
+def header_check(file='ac.csv'):
     with open(file, 'rt') as csvfile:
         header = csv.Sniffer().has_header(csvfile.read(1024))
         return header
 
-#Function to read and write csv file with header
+
+# Function to read and write csv file with header
 def with_header(args):
-    file = args.files.split(',')[0]
-    newfile = args.files.split(',')[0]
-    #Header list creation
+    file = args.file.split(',')[0]
+    newfile = args.file.split(',')[1]
+    # Header list creation
     with open(file) as csv_file:
-        dialect = csv.Sniffer().sniff(csv_file.read(2048)) #To recognise a dialect of csv file
-        csv_file.seek(0) #Move to  the start of the file
+        dialect = csv.Sniffer().sniff(csv_file.read(2048))  # To recognise a dialect of csv file
+        csv_file.seek(0)  # Move to  the start of the file
         reader = csv.reader(csv_file, dialect)
         header = next(reader)
+        print(header)
         csv_file.seek(0)
-        csv_reader = csv.DictReader(csv_file, dialect=dialect) #Creation of Reader as a dictionary with dialect
+        csv_reader = csv.DictReader(csv_file, dialect=dialect)  # Creation of Reader as a dictionary with dialect
+        print(list(csv_reader))
 
-        #Creation of Writer as a dictionary with header
+        # Creation of Writer as a dictionary with header
         with open(newfile, 'w') as new_file:
-            csv_writer = csv.DictWriter(new_file, fieldnames=header)
-            csv_writer.writeheader()
-            #And writing new csv file
+            csv_writer = csv.DictWriter(new_file, fieldnames=header, skipinitialspace=True)
+            # And write new csv file
+            csv_file.seek(0)
+            count = 0
             for line in csv_reader:
-                csv_writer.writerow(line)
-                #print(line)
+                if count == None:
+                    count += 1
+                    continue
+                else:
+                  csv_writer.writerow(line)
+                print(line)
 
     print("List of column names: ", header)
 
-#Function to chose option with or without header
+
+# Function to chose option with or without header
 def start(args):
     if header_check():
         print('With header')
@@ -91,7 +101,6 @@ def row_tables_select(args, delimiter=",", r1=None, r2=None, c1=None, c2=None):
                 writer.writerow(row[c1:c2])
 
 
-
 def extraction(args):
     if args.rows or args.columns:
         if args.rows:
@@ -111,6 +120,7 @@ def extraction(args):
     else:
         row_tables_select(args)
 
+
 def filtering(args):
     c1 = int(args.columns.split(',')[0])
     c2 = int(args.columns.split(',')[1])
@@ -118,20 +128,22 @@ def filtering(args):
     files = args.file.split(',')
     pattern = str(params[1])
     with open(files[0]) as source:
+        dialect = csv.Sniffer().sniff(source.read(1024))  # To recognise a dialect of csv file
+        source.seek(0)  # Move to  the start of the file
+        reader = csv.reader(source, dialect)
         with open(files[1], "w") as dest:
-            reader = csv.reader(source, delimiter=';')
             writer = csv.writer(dest)
             if params[0] == 'c':
                 for row in list(reader):
                     string = ' '.join(row[c1:c2])
-                    print(string)
+                    #print(string)
                     if re.search(pattern, string):
                         writer.writerow(row[c1:c2])
                         print('Mutch')
                     else:
                         print('Dont mutch1')
                         pass
-            else:
+            elif params[0] == 'r':
                 for row in list(reader):
                     string = ','.join(row)
                     if re.search(pattern, string):
@@ -139,67 +151,70 @@ def filtering(args):
                     else:
                         print('Dont mutch')
                         pass
-    #by rows [0-9]+(\.[0-9]+){3}
+
+#by rows [0-9]+(\.[0-9]+){3}
 
 
 def myParser(args):
-    if args.extraction:
-        extraction(args)
-    elif args.filtering:
+    if args.filtering:
         filtering(args)
+
+    elif args.extraction:
+        extraction(args)
     else:
         start(args)
 
 
-
 if __name__ == "__main__":
-
-    #row_tables_select()
-    #start()
+    # row_tables_select()
+    # start()
 
     # parser runtime
-    #start_time = datetime.now()
+    # start_time = datetime.now()
     # logger Initialization
-    #logger = logging.getLogger()
+    # logger = logging.getLogger()
     # My parser variable
     my_parser = argparse.ArgumentParser()
-    #creation of mutually exclusive group
+    # creation of mutually exclusive group
     my_group = my_parser.add_mutually_exclusive_group(required=False)
-    my_group.add_argument('-e', '--extraction', action='store_true', help="Extract special columns or rows. Used with -c, -r")
+    my_group.add_argument('-e', '--extraction', action='store_true',
+                          help="Extract special columns or rows. Used with -c, -r")
     my_group.add_argument('-s', '--filtering', action='store_true', help="To use filter through a file. Used with -p")
 
-    my_parser.add_argument('-p', '--pattern', action='store', type=str, help="Pattern to look for in format c(or r),regex")
-    my_parser.add_argument('-f', '--file', action='store', type=str, help="Source file, destination file", required=True)
-    my_parser.add_argument('-d', '--delimiter', action='store', type=str, help="Specify delimiter next way: '\t' for tab, ';' for ';' etc")
+    my_parser.add_argument('-p', '--pattern', action='store', type=str,
+                           help="Pattern to look for in format c(or r),regex")
+    my_parser.add_argument('-f', '--file', action='store', type=str, help="Source file, destination file",
+                           required=True)
+    my_parser.add_argument('-d', '--delimiter', action='store', type=str,
+                           help="Specify delimiter next way: '\t' for tab, ';' for ';' etc")
     my_parser.add_argument('-r', '--rows', action='store', type=str, help="Specify rows: '1, 2' Use ',' as delimiter ")
-    my_parser.add_argument('-c', '--columns', action='store', type=str, help="Specify columns: (Start, Stop) exemple '1, 2' ")
+    my_parser.add_argument('-c', '--columns', action='store', type=str,
+                           help="Specify columns: (Start, Stop) exemple '1, 2' ")
 
-    #Execute the parse_args() method
+    # Execute the parse_args() method
     args = my_parser.parse_args()
-    #Start of the program
-    #myParser
+    # Start of the program
+    # myParser
     myParser(args)
 
-
-
 '''
-    
+
 
     #Arguments set
-    
-    1) Записать csv в нужном формате____done---
+
+    1) Записать csv в нужном формате____done
     2) Выбока по столбцам_______________done
     3) Выбока по строкам________________done
     4) Фильтр по столбцу________________done
     5) Фильтр по строке_________________done
-    6) Разделитель______________________
-    7) Файл_____________________________
+    6) Разделитель______________________done
+    7) Файл_____________________________done
     8) json_____________________________
 
 
 
 '''
-#Файл CSV должен содержать следующие колонки:
-#columns = ("MAC address", "hostname", "IPv4(null)", "IPv6(null)", "netmask(xxx.xxx.xxx.xxx)", "user login", "full user name", "email", "ssh private key", "ssh public key", "description host", "list of installed app","UUID")
+# Файл CSV должен содержать следующие колонки:
+# columns = ("MAC address", "hostname", "IPv4(null)", "IPv6(null)", "netmask(xxx.xxx.xxx.xxx)", "user login", "full user name", "email", "ssh private key", "ssh public key", "description host", "list of installed app","UUID")
 
 
