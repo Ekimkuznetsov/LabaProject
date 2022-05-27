2275b3f0257b523104130fad10b3f333bce12936
Mykhailo Kuznietsov <ekimkuznetsov@gmail.com>
2022-05-27 12:01:39 +0300

    Task4 Finished

--- laba4.py
+++ laba4.py
@@ -4,24 +4,27 @@ import re
 # from datetime import datetime
 import json
 
-def csv_to_json(args):  # default files names to simplify
-    #files
-
+#csv to json conversation
+def csv_to_json(args):
+    file = args.file.split(',')[0]
+    newfile = args.file.split(',')[1] #json file
+    data = {} #Dict creation
     # Reader creation
     with open(file) as csv_file:
         dialect = csv.Sniffer().sniff(csv_file.read(1024))  # To recognise a dialect of csv file
         csv_file.seek(0)  # Move to  the start of the file
-        reader = csv.reader(csv_file, dialect)
-
-        # Writing to the new file
-        with open(newfile, 'w') as new_file:
-            csv_writer = csv.writer(new_file)
-            for line in reader:
-                csv_writer.writerow(line)
+        csvReader = csv.DictReader(csv_file, dialect=dialect)
+        for rows in csvReader:
+            id = rows['id'] #Dict indexing
+            data[id] = rows
+        # Writing to the new json file
+        with open(newfile, 'w') as json_file:
+            json_file.write(json.dumps(data, ident=4))
     print('Without header')
 
 # good to make property
-def reader_dialected(file='ac.csv'):
+def reader_dialected(args):
+    file = args.file.split(',')[0]
     # Reader creation
     with open(file) as csv_file:
         dialect = csv.Sniffer().sniff(csv_file.read(1024))  # To recognise a dialect of csv file
@@ -31,7 +34,9 @@ def reader_dialected(file='ac.csv'):
 
 
 # Function to write csv in ',' format
-def without_header(file='ac.csv', newfile='new_ac.csv'):  # default files names to simplify
+def without_header(args):
+    file = args.file.split(',')[0]
+    newfile = args.file.split(',')[1]
     # Reader creation
     with open(file) as csv_file:
         dialect = csv.Sniffer().sniff(csv_file.read(1024))  # To recognise a dialect of csv file
@@ -47,7 +52,8 @@ def without_header(file='ac.csv', newfile='new_ac.csv'):  # default files names 
 
 
 # check: Is csv file has header
-def header_check(file='ac.csv'):
+def header_check(args):
+    file = args.file.split(',')[0]
     with open(file, 'rt') as csvfile:
         header = csv.Sniffer().has_header(csvfile.read(1024))
         return header
@@ -87,7 +93,7 @@ def with_header(args):
 
 # Function to chose option with or without header
 def start(args):
-    if header_check():
+    if header_check(args):
         print('With header')
         with_header(args)
     else:
@@ -97,31 +103,24 @@ def start(args):
 
 def row_tables_select(args, delimiter=",", r1=None, r2=None, c1=None, c2=None):
     file = args.file.split(',')[0]
-    print(file)
     newfile = args.file.split(',')[1]
-    print(newfile)
     if args.delimiter:
         delimiter = args.delimiter
-    print(r1, r2, c1, c2)
+
     with open(file) as source:
         dialect = csv.Sniffer().sniff(source.read(2048))  # To recognise a dialect of csv file
         source.seek(0)  # Move to  the start of the file
         reader = csv.reader(source, dialect)
         with open(newfile, "w") as dest:
             writer = csv.writer(dest, delimiter=delimiter)
-            print("wtf")
-            print(reader)
             for row in list(reader)[r1:r2]:
-                print("row")
                 writer.writerow(row[c1:c2])
 
 
 def extraction(args):
     if args.rows or args.columns:
         if args.rows:
-            print("rows is", args.rows)
             rows = args.rows.split(',')
-            print(rows)
             if args.columns:
                 columns = args.columns.split(',')
                 print('Columns presented')
@@ -131,7 +130,6 @@ def extraction(args):
         else:
             columns = args.columns.split(',')
             row_tables_select(args, c1=int(columns[0]), c2=int(columns[1]))
-            print('c1, c2')
     else:
         row_tables_select(args)
 
@@ -174,6 +172,8 @@ def myParser(args):
 
     elif args.extraction:
         extraction(args)
+    elif args.json:
+        csv_to_json(args)
     else:
         start(args)
 
@@ -191,7 +191,7 @@ if __name__ == "__main__":
     my_group.add_argument('-e', '--extraction', action='store_true',
                           help="Extract special columns or rows. Used with -c, -r")
     my_group.add_argument('-s', '--filtering', action='store_true', help="To use filter through a file. Used with -p")
-
+    my_group.add_argument('-j', '--json', action='store_true', help="To to convert to json format. Outer file should be .json")
     my_parser.add_argument('-p', '--pattern', action='store', type=str,
                            help="Pattern to look for in format c(or r),regex")
     my_parser.add_argument('-f', '--file', action='store', type=str, help="Source file, destination file",
@@ -220,7 +220,7 @@ if __name__ == "__main__":
     5) Фильтр по строке_________________done
     6) Разделитель______________________done
     7) Файл_____________________________done
-    8) json_____________________________
+    8) json_____________________________done
 
 
 
